fe9d7a12054e5ed59cd7c4bc8216554e16529067
magarbarsha <magarbarsha333@gmail.com>
2025-03-12 13:08:37 +0545

    makes changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './home'
+import Form2 from './Form'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1>HEll0{1+1}</h1>
-     </>
-      
-  )
+  
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/> 
+        <Route path='/about' element={<h1>this is about page</h1>}/> //path ra element
+        <Route path='/contact' element={<h1>this is contact page</h1>}/> //path ra element
+        <Route path='/Form2' element={<Form2 />}/> //path ra element
+        </Routes>
+      </BrowserRouter>
+    )
 }
 
 export default App
---
+++ src/Form.jsx
@@ -0,0 +1,21 @@
+function Form2(){
+    return (
+        
+        <div>
+          <h1>HTML FORMS</h1>
+          <form>
+          <label>Username:<br/>
+            <input type="text" name="name" placeholder="Enter your name" />
+            </label><br/><br/>
+          <label>Password:<br/>
+            <input type="password" name="pass" placeholder="Enter your password" />
+            </label><br/><br/>
+          
+            <input type="submit" name="submit" value="submit" />
+           
+          </form>
+        </div>
+         
+    )
+}
+export default Form2
